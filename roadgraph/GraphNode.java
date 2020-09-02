4f3439ff6657b1773981f2e453ed8fb212e2d140
Emma Reale <64374768+Emreale20@users.noreply.github.com>
2020-09-02 17:32:49 +0100

    Update GraphNode.java

--- roadgraph/GraphNode.java
+++ roadgraph/GraphNode.java
@@ -10,57 +10,59 @@ import geography.GeographicPoint;
  * @Author - ER
  */
 public class GraphNode {
-
+	
 	private GeographicPoint location;
 	private List<GraphEdge> nextEdges;
 	private List<GraphNode> neighbors;
 	
-	// Constructor sets the location and initialises lists
+	/* Constructor sets the location and initialises lists */
 	public GraphNode () {
 		location = null;
 		nextEdges = new ArrayList<GraphEdge>();
 		neighbors = new ArrayList<GraphNode>();
 	}
 	
+	/* Creates a new node with location specified */
 	public GraphNode (GeographicPoint geoPoint) {
 		location = geoPoint;
 		nextEdges = new ArrayList<GraphEdge>();
 		neighbors = new ArrayList<GraphNode>();
 	}
 	
-	// Adds a neighbour node
+	/* Adds a neighbour node */
 	public void addNeighbor (GraphNode neighbor) {
 		neighbors.add(neighbor);
 	}
 	
+	/* Adds an edge to the node */
 	public void addEdge(GraphEdge edge) {
-		//if (location1 == location) {
-			nextEdges.add(edge);
-		//}
+		nextEdges.add(edge);
 	}
 	
 	//GETTER METHODS
 	
-	// Returns a list of neighbour nodes
+	/* Returns a list of neighbour nodes to be populated 
+	   in MapGraph */
 	public List<GraphNode> getNeighbors() {
 		return neighbors;
 	}
 	
-	// Returns a list of edges (work on this!)
+	/* Returns a list of edges */
 	public List<GraphEdge> getEdges() {
 		return new ArrayList<GraphEdge>(nextEdges);
 	}
 	
-	// Returns the location
+	/* Returns the location */
 	public GeographicPoint getLocation() {
 		return location;
 	}
 	
+	/* Returns the string representation of a node */
 	public String toString() {
 		return ("Location is: " + location);
 	}
 	
-	// For debugging
+	/* For debugging */
 	public static void main(String[] args) {
 		/*GeographicPoint gp = new GeographicPoint(1.0, 1.0);
 		GraphNode newNode = new GraphNode(gp);
