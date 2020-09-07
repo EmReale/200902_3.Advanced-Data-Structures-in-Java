f65558e51809d330dff0f52b4a6137e4e509bb61
Emma Reale <64374768+Emreale20@users.noreply.github.com>
2020-09-07 11:54:30 +0100

    Update greedyBenchmark.java

--- roadgraph - greedySearch extension/greedyBenchmark.java
+++ roadgraph - greedySearch extension/greedyBenchmark.java
@@ -9,8 +9,12 @@ import roadgraph.greedy;
 
 public class greedyBenchmark {
 	
+	// No. of times the method will be called
 	static int trials = 10;
 	
+	/* Method to find which method is more efficient for finding the length of the path;
+	 * Using A* only or by calculating the length of edges between nodes where possible
+	 */
 	public static void main(String [] args) {
 		
 		int index = 0;
@@ -22,30 +26,35 @@ public class greedyBenchmark {
 		float totalTime = 0;
 		float totalTimeA =0;
 		
+		// Creates a new map and populates the path
 		greedy firstMap = new greedy();
 		GraphLoader.loadRoadMap("data/testdata/simpletest.map", firstMap);
 		GeographicPoint start = new GeographicPoint(7.0,3.0);
 		List<GeographicPoint> path = firstMap.greedySearch(start);
 		
+		// Column headings
 		System.out.print ("A* Search  Using Distance between nodes \n");
 		
 		for (int i=0; i<=trials; i++) {
-			
+			// Finds how long it takes to calculate the path length uing A* only
 			startTime = System.nanoTime();
 			firstMap.getTotalLengthAStar(path);
 			endTime = System.nanoTime();
 			timeA = (endTime - startTime);
 			totalTimeA += timeA;
 			
+			// Finds how long it takes to execute the path length using A* 
+			// as little as possible
 			startTime = System.nanoTime();
 			firstMap.getRouteLength(path);
 			endTime = System.nanoTime();
 			time = (endTime - startTime);
 			totalTime += time;
 			
+			// Prints the time for each iteration
 			System.out.println(timeA +"  " + time);
 		}
-		
+		// Returns which method is generally faster and by how much
 		if (totalTime<totalTimeA) {
 			System.out.println("Getting length by adding distance between nodes is on average " + (totalTimeA/totalTime) + " faster");
 		}
